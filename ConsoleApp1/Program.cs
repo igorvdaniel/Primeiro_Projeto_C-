47ce2f25645ca16ffd36c067a1ae3e5fd4b6d348
Igor Veras <igor.veras.daniel@gmail.com>
2026-02-22 21:45:16 -0300

    Aprendendo comandos análogos ao printf e scanf, além de condicionais.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,18 +4,70 @@
     Console.WriteLine(fil);
 }
 
+static void boas_Vindas()
+{
+    //O @ faz a string aparecer literal igual ao console
+    string mensagem = @"
+██████╗░░█████╗░░█████╗░░██████╗  ██╗░░░██╗██╗███╗░░██╗██████╗░░█████╗░░██████╗  ░█████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝  ██║░░░██║██║████╗░██║██╔══██╗██╔══██╗██╔════╝  ██╔══██╗██╔══██╗
+██████╦╝██║░░██║███████║╚█████╗░  ╚██╗░██╔╝██║██╔██╗██║██║░░██║███████║╚█████╗░  ███████║██║░░██║
+██╔══██╗██║░░██║██╔══██║░╚═══██╗  ░╚████╔╝░██║██║╚████║██║░░██║██╔══██║░╚═══██╗  ██╔══██║██║░░██║
+██████╦╝╚█████╔╝██║░░██║██████╔╝  ░░╚██╔╝░░██║██║░╚███║██████╔╝██║░░██║██████╔╝  ██║░░██║╚█████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░  ░░░╚═╝░░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═════╝░  ╚═╝░░╚═╝░╚════╝░
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
+    Console.WriteLine(mensagem);
+}
+
+static void Exibir_Menu()
+{
+    Console.WriteLine("\n1 - registrar banda \n2 - mostrar todas as bandas \n3 - avaliar uma banda \n4 - exibir a avalição média de uma banda \n0 - Sair\n");
+    Console.Write("Digite o número da sua ação:");
+    string opcao_escolhida = Console.ReadLine()!; //O ! faz não receber nulo
+    int opcao = int.Parse(opcao_escolhida);
+
+    if (opcao == 1)
+    {
+        Console.Clear();
+        Console.WriteLine("Você selecionou a opção {0}", opcao);
+        Registrar_Banda();
+    }
+    else if (opcao == 2)
+    {
+        Console.Clear();
+        Console.WriteLine("Você selecionou a opção {0}", opcao);
+        Mostrar_Bandas();
+    }
+    else if (opcao == 3)
+    {
+        Console.Clear();
+        Mostrar_Bandas();
+    }
+}
+
+static void Registrar_Banda()
+{
+    Console.Write("Digite o nome da banda:");
+    string nome_banda = Console.ReadLine()!;
+    Console.Clear();
+    Exibir_Menu();
+}
+
+static void Mostrar_Bandas()
+{
+
+}
+
 static void Main()
 {
     // Screen Sound
-    string mensagem = "Boas vindas ao Screen Sound";
-
-    Console.WriteLine(mensagem);
-    for (int i = 0; i < 10; i++)
-    {
-        Console.WriteLine("Hoje");
-        fila();
-    }
-    Console.ReadLine();
+    boas_Vindas();
+    Exibir_Menu();
 }
 
 Main();
